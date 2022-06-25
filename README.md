2b1428007855a554c7d3c6916ccb18bc5c80fbfe
Dlite <afekhide009@gmail.com>
2022-06-25 14:15:14 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Getting Started With Media Queries
+
+This project is just to learn to create a responsive navigation whilst utilizing @media queries
+
+## Dependencies
+SASS
+React Icons
+
+## Running
+npm install --save
+npm start
---
+++ src/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'menuItem'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'menuItem'
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'menuItem'
+    },
+    {
+        title: 'Works',
+        url: '#',
+        cName: 'menuItem'
+    },
+    {
+        title: 'Appoint',
+        url: '#',
+        cName: 'menuItem'
+    }
+]
